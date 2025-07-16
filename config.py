6299c7ca4005a08ff57f1935b41b951302fa562c
markus2312 <96542767+markus2312@users.noreply.github.com>
2025-07-16 11:49:56 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+SPREADSHEET_ID = "1qQAK-BtK2mfMrZatDlXtGH2JNET8CQi039YO5P8OaUc"
+GOOGLE_CREDENTIALS_FILE = "credentials.json"
+
+# Порты для запуска xray (начинаем с 1080 и +1 для каждого аккаунта)
+XRAY_BASE_PORT = 1080
+
+# Задержка между сообщениями, сек
+MESSAGE_DELAY = 3
+
+# Количество параллельных потоков (аккаунтов)
+CONCURRENT_ACCOUNTS = 3
